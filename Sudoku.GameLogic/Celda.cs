cf403b819f5cffc38ead53267b603019dc879095
BraisLO <blo2000b@gmail.com>
2025-06-14 04:34:07 +0200

    Añadido proyecto Sudoku.GameLogic y clase Celda
    Se agregó el proyecto `Sudoku.GameLogic` al archivo de solución
    `Sudokus.sln` para manejar la lógica del juego.
    
    - Se creó la clase `Celda` en `Celda.cs` con propiedades y métodos
      para representar y gestionar celdas de Sudoku.
    - Se corrigió un error en el método `AsignarValor` relacionado con
      una asignación accidental en la condición `if`.
    - Se configuró el archivo `Sudoku.GameLogic.csproj` para .NET 8.0
      con `using` implícitos y anotaciones de nulabilidad.
    - Se añadieron referencias al proyecto `Sudoku.GameLogic` en
      `Sudokus.ApiService.csproj` y `Sudokus.Web.csproj`.
    - Se actualizaron las configuraciones de compilación en
      `Sudokus.sln` para incluir el nuevo proyecto en los entornos
      `Debug` y `Release`.

---
+++ Sudoku.GameLogic/Celda.cs
@@ -0,0 +1,43 @@
+﻿namespace Sudoku.GameLogic
+{
+    public class Celda
+    {
+        private int x;
+        private int y;
+        public int X => x;
+        public int Y => y;
+
+        private int? valor;
+        public int? Valor => valor;
+
+        public bool esInicial { get; private set; }
+
+        private bool esCongruente;
+        public bool EsCongruente => esCongruente;
+
+
+        public Celda(int x, int y, int? valor)
+        {
+            this.x = x;
+            this.y = y;
+            this.valor = valor;
+            esInicial = valor == null ? false : true;
+            esCongruente = true;
+        }
+
+        public bool AsignarValor(int? nuevoValor)
+        {
+            if (esInicial = true) return false;
+            if (nuevoValor is < 1 or > 9) return false;
+            valor = nuevoValor;
+            return true;
+        }
+
+        public void BorrarValor()
+        {
+            if (!esInicial)
+                valor = null;
+        }
+    }
+    
+}
